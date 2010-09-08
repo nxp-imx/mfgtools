c9ad90aa1a417771f8d6cd13c0b5722485c4137e
Frank Li <Frank.Li@freescale.com>
2010-09-08 16:15:14 +0800

    Add commit hash value to version info
    Auto generate commit hash value to version info
    
    Signed-off-by: Frank Li <Frank.Li@freescale.com>

--- Apps/MfgTool.exe/version.h
+++ Apps/MfgTool.exe/version.h
@@ -2,7 +2,7 @@
 #define STMFGTOOL_MAJOR_VERSION 6
 #define STMFGTOOL_MINOR_VERSION 2
 #define STMFGTOOL_REVISION      013
-#define STMFGTOOL_STRING_VERSION "1.6.2.013\0"
+#define STMFGTOOL_STRING_VERSION "1.6.2.013"
 
 // used by perl script. please update this too
 // STMP_BUILD_VERSION=1.6.2.013
