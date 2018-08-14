d32dae502fe3ae79d7c3ffdb648a2aedf5003353
Frank Li <Frank.Li@nxp.com>
2018-08-14 14:50:16 -0500

    windows recognize appver build number
    Signed-off-by: Frank Li <Frank.Li@nxp.com>

--- msvc/createversion.bat
+++ msvc/createversion.bat
@@ -1,10 +1,14 @@
 call git --version
 IF ERRORLEVEL 1 (
 
-echo #define GIT_VERSION "unknown" > %1/gitversion.h
+echo #define GIT_VERSION "-unknown" > %1/gitversion.h
 
 ) ELSE (
 
 call git log -n1 HEAD --pretty=format:"#define GIT_VERSION \"-g%%%%h\"%%%%n"> %1/gitversion.h
+
+if "%APPVEYOR_BUILD_VERSION%" != "" (
+echo #define BUILD_VER "%APPVEYOR_BUILD_VERSION%.%APPVEYOR_BUILD_NUMBER%" >> %1/gitversion.h
+)	
 
 )
