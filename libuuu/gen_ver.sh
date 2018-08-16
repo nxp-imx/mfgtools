88699dd0beca3f45bfa058c9ad8db57029004e7e
Frank Li <Frank.Li@nxp.com>
2018-08-15 22:39:14 -0500

    use last tag as build version when build from git
    Signed-off-by: Frank Li <Frank.Li@nxp.com>

--- libuuu/gen_ver.sh
+++ libuuu/gen_ver.sh
@@ -9,6 +9,7 @@ then
 	#echo "In a repo"
 	# Get the version of the last commit of the repo
 	version=`git log -n1 HEAD --pretty=format:%h`
+	tag=`git describe --tags`
 else
 	#echo "Not in a repo"
 	version="-unknown"
@@ -27,6 +28,7 @@ echo "$definition" > "$file_to_write"
 if [ "${APPVEYOR_BUILD_VERSION}" = "" ];
 then
 	echo "not build from appveror"
+	echo "#define BUILD_VER \"${tag#*_}\"" >> "$file_to_write"
 else
 	echo "build from appveryor"
 	echo "#define BUILD_VER \"${APPVEYOR_BUILD_VERSION}\"" >> "$file_to_write"
