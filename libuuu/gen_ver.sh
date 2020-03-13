47ddef51d6c24b1f917f348d0f62b4af38852994
Denis Osterland-Heim <Denis.Osterland@diehl.com>
2020-03-13 08:48:58 +0100

    libuuu: allow to build without git
    Read .tarball-version if pressent, instead of git description.
    
    Signed-off-by: Denis Osterland-Heim <Denis.Osterland@diehl.com>

--- libuuu/gen_ver.sh
+++ libuuu/gen_ver.sh
@@ -5,6 +5,12 @@ file_to_write="$1"
 
 set -e
 
+if [ -f ../.tarball-version ]
+then
+	echo "#define GIT_VERSION \"lib$(cat ../.tarball-version)\"" > "$file_to_write"
+	exit 0
+fi
+
 if [ "${APPVEYOR_BUILD_VERSION}" = "" ];
 then
 	echo build not in appveyor
