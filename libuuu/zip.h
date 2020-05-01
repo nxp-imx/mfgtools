94002f4c6fcfe0c0d9ead62b45ec3e25fe5ae8fc
markuspg <markuspg@users.noreply.github.com>
2020-05-01 21:21:13 +0200

    Move functions from zip.h to zip.cpp

--- libuuu/zip.h
+++ libuuu/zip.h
@@ -183,46 +183,12 @@ public:
 class Zip : public Backfile
 {
 public:
-	map<string, Zip_file_Info> m_filemap;
 	int BuildDirInfo();
-	int Open(string filename)
-	{
-		m_filename = filename;
-		return BuildDirInfo();
-	}
+	bool check_file_exist(string filename);
+	int get_file_buff(string filename, shared_ptr<FileBuffer>p);
+	int Open(string filename);
 
-	bool check_file_exist(string filename)
-	{
-		if (m_filemap.find(filename) == m_filemap.end())
-		{
-			string err;
-			err += "Can't find file ";
-			err += filename;
-			set_last_err_string(err);
-			return false;
-		}
-
-		return true;
-	}
-
-	int get_file_buff(string filename, shared_ptr<FileBuffer>p)
-	{
-		if (m_filemap.find(filename) == m_filemap.end())
-		{
-			string err;
-			err += "Can't find file ";
-			err += filename;
-			set_last_err_string(err);
-			return -1;
-		}
-
-		uuu_notify ut;
-		ut.type = uuu_notify::NOTIFY_DECOMPRESS_START;
-		ut.str = (char*)filename.c_str();
-		call_notify(ut);
-
-		return m_filemap[filename].decompress(this, p);
-	}
+	map<string, Zip_file_Info> m_filemap;
 };
 
 
