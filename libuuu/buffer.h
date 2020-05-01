07c63e0fb611fdf1a9a25e08f73a3d3a57f68924
markuspg <markuspg@users.noreply.github.com>
2020-05-01 22:04:49 +0200

    Move non-trivial FileBuffer functions into source

--- libuuu/buffer.h
+++ libuuu/buffer.h
@@ -89,14 +89,7 @@ public:
 		ALLOCATE_VMALLOC,
 	}m_allocate_way;
 
-	int ref_other_buffer(shared_ptr<FileBuffer> p, size_t offset, size_t size)
-	{
-		m_pDatabuffer = p->data() + offset;
-		m_DataSize = m_MemSize = size;
-		m_allocate_way = ALLOCATE_REF;
-		m_ref = p;
-		return 0;
-	}
+	int ref_other_buffer(shared_ptr<FileBuffer> p, size_t offset, size_t size);
 
 	mutex m_async_mutex;
 	
@@ -116,43 +109,9 @@ public:
 	thread m_file_monitor;
 #endif
 
-	FileBuffer()
-	{
-		m_pDatabuffer = NULL;
-		m_DataSize = 0;
-		m_MemSize = 0;
-		m_allocate_way = ALLOCATE_MALLOC;
-		m_dataflags = 0;
-		m_avaible_size = 0;
-	}
-
-	FileBuffer(void*p, size_t sz)
-	{
-		m_pDatabuffer = NULL;
-		m_DataSize = 0;
-		m_allocate_way = ALLOCATE_MALLOC;
-		m_MemSize = 0;
-
-		m_pDatabuffer = (uint8_t*)malloc(sz);
-		m_MemSize = m_DataSize = sz;
-
-		memcpy(m_pDatabuffer, p, sz);
-		m_dataflags = 0;
-	}
-
-	~FileBuffer()
-	{
-		if(m_aync_thread.joinable())
-			m_aync_thread.join();
-
-		if (m_pDatabuffer)
-		{
-			if(m_allocate_way == ALLOCATE_MMAP)
-				unmapfile();
-			if(m_allocate_way == ALLOCATE_MALLOC)
-				free(m_pDatabuffer);
-		}
-	}
+	FileBuffer();
+	FileBuffer(void*p, size_t sz);
+	~FileBuffer();
 
 	int request_data(vector<uint8_t> &data, size_t offset, size_t sz);
 	int request_data(size_t total);
@@ -193,153 +152,15 @@ public:
 	{
 		return (*this)[index];
 	}
-	int resize(size_t sz)
-	{
-		int ret = reserve(sz);
+	int resize(size_t sz);
 
-		m_DataSize = sz;
-		return ret;
-	}
+	int reserve(size_t sz);
 
-	int reserve(size_t sz)
-	{
-		assert(m_allocate_way == ALLOCATE_MALLOC);
+	int swap(FileBuffer & a);
 
-		if (sz > m_MemSize)
-		{
-			m_pDatabuffer = (uint8_t*)realloc(m_pDatabuffer, sz);
-			m_MemSize = sz;
+	int mapfile(string filename, size_t sz);
 
-			if (m_pDatabuffer == NULL)
-			{
-				set_last_err_string("Out of memory\n");
-				return -1;
-			}
-		}
-
-		return 0;
-	}
-
-	int swap(FileBuffer & a)
-	{
-		std::swap(m_pDatabuffer, a.m_pDatabuffer);
-		std::swap(m_DataSize, a.m_DataSize);
-		std::swap(m_MemSize, a.m_MemSize);
-		std::swap(m_allocate_way, a.m_allocate_way);
-
-		return 0;
-	}
-
-	int mapfile(string filename, size_t sz)
-	{
-#ifdef _MSC_VER
-		
-		m_Request.StructureVersion = REQUEST_OPLOCK_CURRENT_VERSION;
-		m_Request.StructureLength = sizeof(REQUEST_OPLOCK_INPUT_BUFFER);
-		m_Request.RequestedOplockLevel = (OPLOCK_LEVEL_CACHE_READ | OPLOCK_LEVEL_CACHE_HANDLE);
-		m_Request.Flags = REQUEST_OPLOCK_INPUT_FLAG_REQUEST;
-		
-		REQUEST_OPLOCK_OUTPUT_BUFFER Response;
-
-		m_OverLapped.hEvent = CreateEvent(NULL, TRUE, FALSE, NULL);
-		ResetEvent(m_OverLapped.hEvent);
-
-		m_file_handle = CreateFile(filename.c_str(),
-			GENERIC_READ,
-			FILE_SHARE_READ | FILE_SHARE_WRITE,
-			NULL,
-			OPEN_EXISTING,
-			FILE_ATTRIBUTE_NORMAL | FILE_FLAG_RANDOM_ACCESS | FILE_FLAG_OVERLAPPED,
-			NULL);
-
-		if (m_file_handle == INVALID_HANDLE_VALUE)
-		{
-			string err = "Create File Failure ";
-			err += filename;
-			set_last_err_string(err);
-			return -1;
-		}
-
-		BOOL bSuccess = DeviceIoControl(m_file_handle,
-			FSCTL_REQUEST_OPLOCK,
-			&m_Request,
-			sizeof(m_Request),
-			&Response,
-			sizeof(Response),
-			NULL,
-			&m_OverLapped);
-
-		if (bSuccess || GetLastError() == ERROR_IO_PENDING)
-		{
-			m_file_monitor = thread(file_overwrite_monitor, filename, this);
-		}
-
-		m_file_map = CreateFileMapping(m_file_handle,
-			NULL, PAGE_READONLY, 0, 0, NULL);
-
-		if (m_file_map == INVALID_HANDLE_VALUE)
-		{
-			set_last_err_string("Fail create Map");
-			return -1;
-		}
-
-		m_pDatabuffer = (uint8_t *)MapViewOfFile(m_file_map, FILE_MAP_READ, 0, 0, sz);
-		m_DataSize = sz;
-		m_MemSize = sz;
-		m_allocate_way = ALLOCATE_MMAP;
-		
-#else
-		int fd = open(filename.c_str(), O_RDONLY);
-		if (fd == -1)
-		{
-			string err;
-			err += "xx Failure open file: ";
-			err + filename;
-			set_last_err_string(err);
-			return -1;
-		}
-
-		m_pDatabuffer = (uint8_t *)mmap64(0, sz, PROT_READ, MAP_SHARED, fd, 0);
-		if (m_pDatabuffer == MAP_FAILED) {
-			m_pDatabuffer = NULL;
-			set_last_err_string("mmap failure\n");
-			return -1;
-		}
-		m_DataSize = sz;
-		m_MemSize = sz;
-		m_allocate_way = ALLOCATE_MMAP;
-		close(fd);
-#endif
-		if (m_pDatabuffer)
-			return 0;
-
-		set_last_err_string("mmap file failure");
-		return -1;
-	}
-
-	int unmapfile()
-	{
-		if (m_pDatabuffer)
-		{
-#ifdef _MSC_VER
-			UnmapViewOfFile(m_pDatabuffer);
-			m_pDatabuffer = NULL;
-			CloseHandle(m_file_map);
-			CloseHandle(m_file_handle);
-			SetEvent(m_OverLapped.hEvent);
-			
-			if (m_file_monitor.joinable())
-				m_file_monitor.join();
-
-			CloseHandle(m_OverLapped.hEvent);
-			m_OverLapped.hEvent = m_file_map = m_file_handle = INVALID_HANDLE_VALUE;
-#else
-			munmap(m_pDatabuffer, m_DataSize);
-#endif
-			m_pDatabuffer = NULL;
-		}
-		return 0;
-	}
+	int unmapfile();
 	//Read write lock;
 	uint64_t m_timesample;
 	int reload(string filename, bool async=false);
