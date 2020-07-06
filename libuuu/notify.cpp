e17518fc3c5550f4702a83d447e570b97d1cd0e3
markuspg <markuspg@users.noreply.github.com>
2020-07-06 09:06:51 +0200

    Simplify notify.cpp
    * drop notify_map structure
    * reduce includes
    * replace vector by map
    * return meaningful values from functions
    
    The change from vector to map has only one caveat. While with the vector
    the notifications are done in insertion order with the map the
    notifications are done in the order of the function pointers' values.

--- libuuu/notify.cpp
+++ libuuu/notify.cpp
@@ -29,81 +29,49 @@
 *
 */
 
-#include <string>
-#include <vector>
+#include "libuuu.h"
+
+#include <chrono>
+#include <iostream>
+#include <map>
+#include <mutex>
+#include <thread>
 
 using namespace std;
 
-#include "libuuu.h"
-#include "liberror.h"
-#include <thread>
-#include <mutex>
-#include <iostream>
-#include <time.h>
-#include <chrono>
-#include <ctime>
-
-struct  notify_map
-{
-	uuu_notify_fun f;
-	void *data;
-};
-
-static vector<struct notify_map> g_notify_callback_list;
-static mutex g_mutex_nofity;
+static map<uuu_notify_fun, void*> g_notification_map;
+static mutex g_mutex_notify;
 
 using namespace std::chrono;
-static time_point<steady_clock> g_now = steady_clock::now();
+static const time_point<steady_clock> g_now = steady_clock::now();
 
 int uuu_register_notify_callback(uuu_notify_fun f, void *data)
 {
-	notify_map a;
-	a.f = f;
-	a.data = data;
+	std::lock_guard<mutex> lock(g_mutex_notify);
 
-	std::lock_guard<mutex> lock(g_mutex_nofity);
-
-	for (size_t i = 0; i < g_notify_callback_list.size(); i++)
-	{
-		if (g_notify_callback_list[i].f == f)
-			return 0;
-	}
-
-	g_notify_callback_list.push_back(a);
-	return 0;
+	return g_notification_map.emplace(f, data).second ? 0 : 1;
 }
 
 int uuu_unregister_notify_callback(uuu_notify_fun f)
 {
-	vector<struct notify_map>::iterator it=g_notify_callback_list.begin();
+	std::lock_guard<mutex> lock(g_mutex_notify);
 
-	std::lock_guard<mutex> lock(g_mutex_nofity);
-
-	for (;it!=g_notify_callback_list.end();it++)
-	{
-		if (it->f == f)
-		{
-			g_notify_callback_list.erase(it);
-			return 0;
-		}
-	}
-	return 0;
+	return g_notification_map.erase(f) > 0 ? 0 : 1;
 }
 
 void call_notify(struct uuu_notify nf)
 {
 	//Change RW lock later;
-	std::lock_guard<mutex> lock(g_mutex_nofity);
+	std::lock_guard<mutex> lock(g_mutex_notify);
 
-	vector<struct notify_map>::iterator it = g_notify_callback_list.begin();
 	nf.id = std::hash<std::thread::id>{}(std::this_thread::get_id());
+	nf.timestamp = static_cast<uint64_t>(
+		duration_cast<milliseconds>(steady_clock::now() - g_now).count());
 
-	nf.timestamp = duration_cast<milliseconds>(steady_clock::now() - g_now).count();
-
-	for (; it != g_notify_callback_list.end(); it++)
+	for (const auto &item : g_notification_map)
 	{
 		try {
-			it->f(nf, it->data);
+			item.first(nf, item.second);
 		} catch (const std::exception& e) {
 			std::cerr << "notify exception: " << e.what() << std::endl;
 		}
