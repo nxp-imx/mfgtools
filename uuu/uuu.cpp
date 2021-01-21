86f82c2af018a4a2d325f3237f40e967b0c3d720
Guido Günther <agx@sigxcpu.org>
2021-01-21 16:24:35 +0100

    uuu: Use uaccess instead of '0666' in the udev rule
    This gives access to the user in the active session instead of opening
    the devices up to everybody which is inherently insecure.

--- uuu/uuu.cpp
+++ uuu/uuu.cpp
@@ -204,7 +204,7 @@ int print_cfg(const char *pro, const char * chip, const char * /*compatible*/, u
 int print_udev_rule(const char * /*pro*/, const char * /*chip*/, const char * /*compatible*/,
 	uint16_t vid, uint16_t pid, uint16_t /*bcdmin*/, uint16_t /*bcdmax*/, void * /*p*/)
 {
-	printf("SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"%04x\", ATTRS{idProduct}==\"%04x\", MODE=\"0666\"\n",
+	printf("SUBSYSTEM==\"usb\", ATTRS{idVendor}==\"%04x\", ATTRS{idProduct}==\"%04x\", TAG+=\"uaccess\"\n",
 			vid, pid);
 	return 0;
 }
