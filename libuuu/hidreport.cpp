26d4b25d23be55b4dda16d166954eaa1ab912dfe
Marco Felsch <m.felsch@pengutronix.de>
2023-11-10 10:46:07 +0100

    Revert "Fixed sdps crash for some size flash.bin"
    This reverts commit de317f587003dbd6203af1c647e2fccb912f8f0d.
    
    The upcoming patch will fix the issue more straight forward.
    
    Signed-off-by: Marco Felsch <m.felsch@pengutronix.de>

--- libuuu/hidreport.cpp
+++ libuuu/hidreport.cpp
@@ -78,10 +78,6 @@ int HIDReport::write(const void *p, size_t sz, uint8_t report_id)
 		m_out_buff[0] = report_id;
 
 		size_t s = sz - off;
-		size_t copy_sz = s;
-
-		if (copy_sz > m_size_out)
-			copy_sz = m_size_out;
 
 		/*
 		 * The Windows HIDAPI is ver strict. It always require to send
@@ -92,8 +88,7 @@ int HIDReport::write(const void *p, size_t sz, uint8_t report_id)
 		if (s > m_size_out || report_id == 2)
 			s = m_size_out;
 
-		/* copy_sz can't be bigger then input data size, otherwise access unpaged memory */
-		memcpy(m_out_buff.data() + m_size_payload, buff + off, copy_sz);
+		memcpy(m_out_buff.data() + m_size_payload, buff + off, s);
 
 		int ret = m_pdev->write(m_out_buff.data(), s + m_size_payload);
 
