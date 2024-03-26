254123a427f7663cfd764bdba5e954a82d3de4e5
Alessandro Zini <alezini94@hotmail.it>
2024-03-26 15:19:27 +0100

    trans: unmask libusb errors
    When using uuu it can happen that libusb operations sometimes return with an error code, which is currently masked.
    
    This prints the error code and propagates it.

--- libuuu/trans.cpp
+++ libuuu/trans.cpp
@@ -30,6 +30,7 @@
 */
 
 #include "trans.h"
+#include "libcomm.h"
 #include "libuuu.h"
 #include "liberror.h"
 #include "libusb.h"
@@ -97,28 +98,35 @@ int TransBase::read(vector<uint8_t> &buff)
 int USBTrans::open(void *p)
 {
 	m_devhandle = p;
+	string_ex err;
+	int libusb_ret = 0;
 	libusb_device_handle * handle = (libusb_device_handle *)m_devhandle;
+
 	if (libusb_kernel_driver_active(handle, 0))
 	{
-		int ret = libusb_detach_kernel_driver((libusb_device_handle *)m_devhandle, 0);
-		if(ret <0 && ret != LIBUSB_ERROR_NOT_SUPPORTED)
+		libusb_ret = libusb_detach_kernel_driver((libusb_device_handle *)m_devhandle, 0);
+
+		if(libusb_ret < 0 && libusb_ret != LIBUSB_ERROR_NOT_SUPPORTED)
 		{
-			set_last_err_string("detach kernel driver failure");
-			return -1;
+			err.format("detach kernel driver failure (%d)", libusb_ret);
+			set_last_err_string(err);
+			return libusb_ret;
 		}
 	}
 
-	if (libusb_claim_interface(handle, 0))
+	if ((libusb_ret = libusb_claim_interface(handle, 0)))
 	{
-		set_last_err_string("Failure claim interface");
-		return -1;
+		err.format("Failure claim interface (%d)", libusb_ret);
+		set_last_err_string(err);
+		return libusb_ret;
 	}
 
 	libusb_config_descriptor *config;
-	if (libusb_get_active_config_descriptor(libusb_get_device(handle), &config))
+	if ((libusb_ret = libusb_get_active_config_descriptor(libusb_get_device(handle), &config)))
 	{
-		set_last_err_string("Can't get config descriptor");
-		return -1;
+		err.format("Can't get config descriptor (%d)", libusb_ret);
+		set_last_err_string(err);
+		return libusb_ret;
 	}
 
 	m_EPs.clear();
@@ -143,8 +151,10 @@ int USBTrans::close()
 
 int HIDTrans::open(void *p)
 {
-	if (USBTrans::open(p))
-		return -1;
+	int ret;
+
+	if ((ret = USBTrans::open(p)))
+		return ret;
 
 	for (const auto &ep : m_EPs)
 	{
@@ -188,9 +198,8 @@ int HIDTrans::write_simple(void *buff, size_t size)
 
 	if (ret < 0)
 	{
-		string err;
-		err = "HID(W):";
-		err += libusb_error_name(ret);
+		string_ex err;
+		err.format("HID(W): %s (%d)", libusb_error_name(ret), ret);
 		set_last_err_string(err);
 		return ret;
 	}
@@ -215,10 +224,8 @@ int HIDTrans::read_simple(void *buff, size_t size, size_t *rsize)
 
 	if (ret < 0)
 	{
-		string error;
-		string err;
-		err = "HID(R):";
-		err += libusb_error_name(ret);
+		string_ex err;
+		err.format("HID(R): %s (%d)", libusb_error_name(ret), ret);
 		set_last_err_string(err);
 		return ret;
 	}
@@ -250,10 +257,8 @@ int BulkTrans::write_simple(void *buff, size_t size)
 
 		if (ret < 0)
 		{
-			string error;
-			string err;
-			err = "Bulk(W):";
-			err += libusb_error_name(ret);
+			string_ex err;
+			err.format("Bulk(W): %s (%d)", libusb_error_name(ret), ret);
 			set_last_err_string(err);
 			return ret;
 		}
@@ -273,10 +278,8 @@ int BulkTrans::write_simple(void *buff, size_t size)
 
 		if (ret < 0)
 		{
-			string error;
-			string err;
-			err = "Bulk(W):";
-			err += libusb_error_name(ret);
+			string_ex err;
+			err.format("Bulk(W): %s (%d)", libusb_error_name(ret), ret);
 			set_last_err_string(err);
 			return ret;
 		}
@@ -287,8 +290,10 @@ int BulkTrans::write_simple(void *buff, size_t size)
 
 int BulkTrans::open(void *p)
 {
-	if (USBTrans::open(p))
-		return -1;
+	int ret;
+
+	if ((ret = USBTrans::open(p)))
+		return ret;
 
 	for (size_t i = 0; i < m_EPs.size(); i++)
 	{
@@ -327,10 +332,8 @@ int BulkTrans::read_simple(void *buff, size_t size, size_t *rsize)
 
 	if (ret < 0)
 	{
-		string error;
-		string err;
-		err = "Bulk(R):";
-		err += libusb_error_name(ret);
+		string_ex err;
+		err.format("Bulk(R): %s (%d)", libusb_error_name(ret), ret);
 		set_last_err_string(err);
 		return ret;
 	}
